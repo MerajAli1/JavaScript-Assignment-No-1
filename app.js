f33f2ad9907b89e88a099e40236ba00232846846
Karizali <merajali45425@gmail.com>
2022-10-29 19:35:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// Chapter 1 starts
+alert("My Name Is Meraj Ali");
+// Chapter 1 ends
+// Chapter 2 Starts
+var name = "This is My First JavaScript Program";
+alert(name)
+alert('Here is some of my practice Click on OK')
+// Chapter 2 ends
+// Chapter 3 starts
+var name_1 = 10;
+var name_2 = 20;
+total =name_1 + name_2
+alert(total)
+var name_1 = 10;
+var name_2 = 20;
+total =name_2 - name_1
+alert(total)
+var name_1 = 10;
+var name_2 = 20;
+total =name_1/name_2
+alert(total)
+var name_1 = 10;
+var name_2 = 20;
+total =name_1*name_2
+alert(total)
+// Chapter 3 ends
+// Chapter 4 starts
+                    // legal Variable:
+alert("These are some legal variables: name, Name, name1, _name, Name, nAme")
+// name 
+// Name 
+// name1
+// _name 
+// Name 
+// nAme 
+alert("These are some illegal variables: 1name, @name, name@, n@me")
+                    // illegal Variable:
+// 1name
+// @name
+// name@
+// n@me
+// Chapter 4 ends
+// Chapter 5 starts
+var xyz = 50;
+total = xyz + 10
+alert(total)
+// Chapter 5 ends
+// Chapter 6 starts
+var num1 = 20;
+num1++;
+alert(num1)
+console.log(num1)
+// Chapter 6 ends
+// Chapter 7 starts
+var amount = 1 - 6 / 4;
+alert(amount)
+console.log(amount)
+var amount = (6 * 6)/4 + 6;
+alert(amount)
+console.log(amount)
+// Chapter 7 ends
+// Chapter 8 starts
+var username = "For your Input"
+alert("Thanks, " + username + "!");
+// Chapter 8 ends
+
+
+
